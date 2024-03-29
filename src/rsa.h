c260a4150dc70d34aec7f24fcfac96b711561ddd
engesolda <ryan12111@gmail.com>
2024-03-29 12:15:31 -0600

    Initial Commit

---
+++ src/rsa.h
@@ -0,0 +1,25 @@
+// Copyright 2023 The Forgotten Server Authors. All rights reserved.
+// Use of this source code is governed by the GPL-2.0 License that can be found in the LICENSE file.
+
+#ifndef FS_RSA_H
+#define FS_RSA_H
+
+class RSA
+{
+public:
+	static constexpr int16_t BUFFER_LENGTH = 128;
+
+	RSA() = default;
+
+	// non-copyable
+	RSA(const RSA&) = delete;
+	RSA& operator=(const RSA&) = delete;
+
+	void loadPEM(const std::string& filename);
+	void decrypt(char* msg) const;
+
+private:
+	CryptoPP::RSA::PrivateKey pk;
+};
+
+#endif // FS_RSA_H
